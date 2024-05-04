69fc822c2c78a98c418a5abb36e8309ae3f68e96
oynozan <oynozan@hotmail.com>
2024-05-04 08:44:28 +0300

    initial commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="not-found">
+            <p className="idk">¯\_(ツ)_/¯</p>
+            <span>The page you&#39;re looking for doesn&#39;t exist</span>
+        </div>
+    )
+}
